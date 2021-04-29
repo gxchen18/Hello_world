7ed3fac383d965c9bc994a267c5430e41baa78cb
gxchen <cgx18@mails.tsinghua.edu.cn>
2021-04-29 11:51:07 +0800

    Add hello world script by python

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+#import matplotlib.pyplot as plt
+
+print('hello world')
+
+#x = np.linspace(0,10,100)
+#y = np.sin(x)
+#plt.plot(x,y,'--',marker='o')
+#plt.show()
